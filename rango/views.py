a5757e5e89e4e9e397be8bff900de524fb69806b
James Mushahwar <2197249m@student.gla.ac.uk>
2017-02-06 14:49:42 +0000

    Chapter 6 in progress, editted views and inde.html

--- rango/views.py
+++ rango/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render
+
+# Import the category model
+from rango.models import Category
 
 def index(request):
-	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-	return render(request, 'rango/index.html', context=context_dict)
-    # return HttpResponse("Rango says hey there partner!" + "<br/> <a href='/rango/about/'>About</a>")
+	# Query the database for a list of ALL categories currently stored.
+	# Order the categories by no. likes in descending order.
+	# Retrieve the top 5 only - or all if less than 5.
+	# Place the list in our context_dict dictionary
+	# that will be passed to the template engine.
+	category_list = Category.objects.order_by('-likes')[:5]
+	context_dict = {'categories': category_list}
+	# Render the response and send it back!
+	return render(request, 'rango/index.html', context_dict)
+
 
 def about(request):
 	context_dict = {'boldmessage':"This tutorial has been put together by James Mushahwar"}
 	return render(request, 'rango/about.html', context=context_dict)
-	# return HttpResponse('Rango says here is the about page.' + '<br/> <a href="/rango/">Index</a>')
+	
