7a03cdd0676646fea49741132a707b07362d060b
James Mushahwar <2197249m@student.gla.ac.uk>
2017-02-06 13:24:15 +0000

    End of Chapter 5.7- 1 Error: The Object titles only populate on the cmd terminal and on the django application as 'Page object'.

--- populate_rango.py
+++ populate_rango.py
@@ -1,6 +1,5 @@
 import os 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-						'settings') 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','tango_with_django_project.settings') 
  
 import django
 django.setup()
@@ -30,21 +29,20 @@ def populate():
 		"url":"http://bottlepy.org/docs/dev/"}, 
 		{"title":"Flask", 
 		"url":"http://flask.pocoo.org"} ] 
-			
+		
 	cats = {"Python": {"pages": python_pages}, 
-		"Django": {"pages": django_pages}, 
-		"Other Frameworks": {"pages": other_pages} } 
- 
+			"Django": {"pages": django_pages}, 
+			"Other Frameworks": {"pages": other_pages} } 
  
 
-for cat, cat_data in cats.items(): 
-	c = add_cat(cat) 
-	for p in cat_data["pages"]: 
-		add_page(c, p["title"], p["url"]) 
+	for cat, cat_data in cats.items(): 
+		c = add_cat(cat) 
+		for p in cat_data["pages"]: 
+			add_page(c, p["title"], p["url"]) 
   
-for c in Category.objects.all(): 
-	for p in Page.objects.filter(category=c):
-		print("- {0} - {1}".format(str(c), str(p))) 
+	for c in Category.objects.all(): 
+		for p in Page.objects.filter(category=c):
+			print("- {0} - {1}".format(str(c), str(p))) 
 
 def add_page(cat, title, url, views=0): 
 	p = Page.objects.get_or_create(category=cat, title=title)[0] 
