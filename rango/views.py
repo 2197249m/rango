1437d9ad207907ffcf3842b70befa953c8b6e11a
James Mushahwar <2197249m@student.gla.ac.uk>
2017-02-08 14:39:46 +0000

    Chapter 8 in progress

--- rango/views.py
+++ rango/views.py
@@ -46,8 +46,14 @@ def show_category(request, category_name_slug):
 	return render(request, 'rango/category.html', context_dict)	
 
 def about(request):
-	context_dict = {'boldmessage':"This tutorial has been put together by James Mushahwar"}
-	return render(request, 'rango/about.html', context=context_dict)
+	#context_dict = {'boldmessage':"This tutorial has been put together by James Mushahwar"}
+	#return render(request, 'rango/about.html', context=context_dict)
+	
+	# prints out whether the method is a GET or a POST
+    print(request.method)
+    # prints out the user name, if no one is logged in it prints `AnonymousUser`
+    print(request.user)
+    return render(request, 'rango/about.html', {})
 	
 def add_category(request):
     form = CategoryForm()
